7ae8056cc925b5baea366ad3ea5c9bc46d88c881
Anton Karmanov <anton@karmanov.ws>
2018-09-01 14:33:54 +0300

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,16 @@ module.exports = {
     ],
     rules: {
         'no-new': 0,
-        "react/jsx-indent": ["error", 4],
-        "indent": ["error", 4],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "max-len": 0,
         "no-console": 1,
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": [2, 4],
+        // temporary disable
+        "jsx-a11y/anchor-is-valid": 0,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "jsx-a11y/no-static-element-interactions": 0,
+        "jsx-a11y/label-has-for": 0,
+        "jsx-a11y/label-has-associated-control": 0,
     },
 };
